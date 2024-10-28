0a2a8bfbe717a44103a133ee0acf45d47b95c73f
Rajiv Kumar Kale <rajivkumarkale@gmail.com>
2024-10-28 20:51:07 +0530

    logging and assertions added

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,7 +1,14 @@
-#include <test.h>
+#include <core/logger.h>
+#include <core/asserts.h>
 
 int main(void) {
-    print_int(42);
-    print_int(17);
+    KFATAL("just testing %d ,%d", 42, 17);
+    KERROR("just testing %d ,%d", 42, 17);
+    KWARN("just testing %d ,%d", 42, 17);
+    KINFO("just testing %d ,%d", 42, 17);
+    KTRACE("just testing %d ,%d", 42, 17);
+
+    KASSERT(1 == 0);
+    KASSERT_MSG(1 == 0, "Testing the assert msg");
     return 0;
 }
