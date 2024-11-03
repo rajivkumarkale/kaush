ab1136140bc15041b8f619a1c6f853e336b6ba2c
Rajiv Kumar Kale <rajivkumarkale@gmail.com>
2024-11-03 20:53:12 +0530

    lightweight event system added

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -3,6 +3,7 @@
 #include "logger.h"
 #include "platform/platform.h"
 #include "core/kmemory.h"
+#include "core/event.h"
 #include "game_types.h"
 
 typedef struct application_state {
@@ -40,6 +41,11 @@ b8 application_create(game* game_inst) {
 
     app_state.is_running = TRUE;
     app_state.is_suspended = FALSE;
+
+    if (!event_initialize()) {
+        KERROR("Event system failed initialization. Application cannot continue.");
+        return FALSE;
+    }
 
     if (!platform_startup(
             &app_state.platform,
@@ -88,6 +94,9 @@ b8 application_run() {
     }
 
     app_state.is_running = FALSE;
+
+    event_shutdown();
+
     platform_shutdown(&app_state.platform);
 
     return TRUE;
