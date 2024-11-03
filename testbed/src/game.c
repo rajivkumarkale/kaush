b7fbc811ea6b6ecb677f4ccc81457f5ab42d04df
Rajiv Kumar Kale <rajivkumarkale@gmail.com>
2024-11-03 17:43:26 +0530

    dynamic arrays added

--- testbed/src/game.c
+++ testbed/src/game.c
@@ -1,9 +1,37 @@
 #include "game.h"
+#include <containers/darray.h>
 
 #include <core/logger.h>
 
 b8 game_initialize(game* game_inst) {
     KDEBUG("game_initialize() called!");
+
+    u32* arr = (u32*)darray_reserve(u32, 5);
+    for (u32 i = 0; i < 5; i++) {
+        darray_push(arr, i * 2);
+    }
+
+    darray_push(arr, 7);
+    darray_push(arr, 9);
+
+    for (u32 i = 0; i < darray_length(arr); i++) {
+        KDEBUG("%d", arr[i]);
+    }
+    KDEBUG("----");
+
+    u32 pop_value;
+    darray_pop(arr, &pop_value);
+    KDEBUG("%d", pop_value);
+
+    darray_pop_at(arr, 3, &pop_value);
+    KDEBUG("%d", pop_value);
+
+    KDEBUG("----");
+
+    for (u32 i = 0; i < darray_length(arr); i++) {
+        KDEBUG("%d", arr[i]);
+    }
+
     return TRUE;
 }
 
